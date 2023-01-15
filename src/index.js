02bed184cdb3b832412ce51f17bda57776f7b568
Leonid Marz <l3m4rz@gmail.com>
2023-01-15 20:01:14 +0300

    feat: prettier added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HashRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <HashRouter>
-      <React.StrictMode>
-         <App/>
-      </React.StrictMode>
-   </HashRouter>
+  <HashRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </HashRouter>
 );
-
